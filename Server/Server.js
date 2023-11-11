d7e0bd5fb2892ddc71389f8cd53737aed0f6b0a8
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2023-11-11 13:15:17 +0500

    Server setup

---
+++ Server/Server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const PORT = process.env.PORT || 3000;
+
+
+const server = http.createServer((req, res) => {
+    res.end('Hello World');
+});
+
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
